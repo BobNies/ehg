276140b45b3c1e5402a9ea03b8ad18057757e93b
Josh Nies <whatsthatfunction@gmail.com>
2018-05-08 22:24:31 -0700

    progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import CustomNavBar from './CustomNavBar'
+import Img from 'react-image'
 
 class Home extends Component {
   render () {
     return(
       <div>
         <CustomNavBar/>
-        
+        <div className='home-img-1-crop'>
+          <Img className='home-img-1' src='example-photo-3.jpeg'/>
+        </div>
+        <h2 className='home-title-1'>A collective gallery of mixed media art.</h2>
       </div>
     );
   }
