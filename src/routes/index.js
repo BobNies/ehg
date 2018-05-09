44dd9d8ed03a4381719f95a195349fb19c0b3cb7
Josh Nies <whatsthatfunction@gmail.com>
2018-05-08 20:43:56 -0700

    added 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import Gallery from '../components/Gallery';
 import Contact from '../components/Contact';
+import NotFound from '../components/NotFound';
 
 export default () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ export default () => (
       <Route exact path='/' component={Home}/>
       <Route path='/artists/:artistName/gallery' component={Gallery}/>
       <Route path='/artists/:artistName/contact' component={Contact}/>
+      <Route component={NotFound}/>
     </Switch>
   </BrowserRouter>
 );
