fba05105e19c437fbdd17c3db563df7a2b47e1a3
Josh Nies <whatsthatfunction@gmail.com>
2018-05-08 17:34:30 -0700

    more progress on navbar

--- src/components/CustomNavBar.js
+++ src/components/CustomNavBar.js
@@ -7,7 +7,7 @@ class CustomNavBar extends Component {
       <div className="navbar">
         <h2><Link to='/artists/fred-briscoe' className='navbar-link'>FRED BRISCOE</Link></h2>
         <h1><Link to='/' className='navbar-title'>EUCALYPTUS HILLS GALLERY</Link></h1>
-        <h2><Link to='/artists/mike-roser' className='navbar-link'>MIKE ROSER</Link></h2>
+        <h2><Link to='/artists/michael-roser' className='navbar-link'>MICHAEL ROSER</Link></h2>
       </div>
     );
   }
