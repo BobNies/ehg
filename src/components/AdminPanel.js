bd4c39fe7877dd671876475dcf04ebede3485694
Josh Nies <whatsthatfunction@gmail.com>
2018-05-12 20:00:29 -0700

    progress on gallery item addition

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -13,8 +13,21 @@ class AdminPanel extends Component {
     super(props);
 
     this.state = {
-      inputInstaPost: ''
+      inputInstaPost: '',
+      inputGalleryItem: {
+        name: '',
+        artist: '',
+        imageLink: '',
+        description: '',
+        sold: false,
+        image: null
+      },
+      isUploading: false,
+      uploadProgress: 0,
+      uploadError: null
     }
+
+    this.instagramPostControl = React.createRef();
   }
 
   applySettings(produceNotification, setIntagramPost) {
@@ -27,6 +40,10 @@ class AdminPanel extends Component {
     produceNotification('Update Successful', 'Yay!', 'success');
   }
 
+  addGalleryItem() {
+    console.log('Adding gallery item with name of', this.state.inputGalleryItem.name);
+  }
+
   render () {
     return (
       <Consumer>
@@ -34,21 +51,89 @@ class AdminPanel extends Component {
           const { user, logOut, produceNotification, setIntagramPost } = value;
           return user ? (
             <div>
-              {/* <NotificationSystem ref={this.notificationSystem} /> */}
               <CustomNavBar />
               <div className='admin'>
                 <h1>ADMIN PANEL</h1>
                 <Grid className='admin-grid'>
-                  <Row className='admin-row admin-row-first'>
+                  <Row className='admin-row admin-row-header admin-row-first'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h2>Instagram</h2>
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
                     <Col xs={6} md={4} mdOffset={2}>
                       <h4>Promoted Instagram Post</h4>
                     </Col>
                     <Col xs={6} md={4}>
                       <FormControl
+                        ref={this.instagramPostControl}
                         type='text'
-                        placeholder='URL (post address)'
+                        placeholder='URL (link)'
                         onChange={event => this.setState({ inputInstaPost: event.target.value })}
                         />
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row className='admin-row admin-row-header'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h2>New Gallery Item</h2>
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h4>Name</h4>
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <FormControl
+                        ref={this.instagramPostControl}
+                        type='text'
+                        placeholder='Name'
+                        onChange={event => this.setState({ inputGalleryItem: {name: event.target.value} })}
+                        />
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h4>Artist</h4>
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <FormControl
+                        ref={this.instagramPostControl}
+                        type='text'
+                        placeholder='Artist'
+                        onChange={event => this.setState({ inputGalleryItem: {artist: event.target.value} })}
+                        />
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h4>Description</h4>
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <FormControl
+                        ref={this.instagramPostControl}
+                        type='text'
+                        placeholder='Description'
+                        onChange={event => this.setState({ inputGalleryItem: {description: event.target.value} })}
+                        />
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
+                    <Col xs={6} md={4} mdOffset={2}>
+                      <h4>Image</h4>
+                    </Col>
+                    <Col xs={6} md={4}>
+                      <input type='file' onChange={event => this.setState({ inputGalleryItem: {image: event.target.files[0]} })} />
+                    </Col>
+                  </Row>
+                  <Row className='admin-row'>
+                    <Col xs={6} md={4} mdOffset={6}>
+                      <Button
+                        bsStyle='primary'
+                        onClick={() => this.addGalleryItem()}
+                        >
+                        Add
+                      </Button>
                     </Col>
                   </Row>
                   <hr />
