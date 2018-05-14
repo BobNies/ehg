f05824ab03ba5d0fa71c0b03858cf1e7d4728d05
Josh Nies <whatsthatfunction@gmail.com>
2018-05-14 11:13:59 -0700

    https redirect react package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Routes from './routes';
 import { Provider } from './MyContext';
+import HttpsRedirect from 'react-https-redirect';
 
 /* Master component.  Holds initial state variables. */
 class App extends Component {
   render() {
     return (
       <Provider>
-        <Routes />
+        <HttpsRedirect>
+          <Routes />
+        </HttpsRedirect>
       </Provider>
     )
   }
