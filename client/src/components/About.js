5f06bf45c345593146970bbd319ddda725f86c81
Josh Nies <whatsthatfunction@gmail.com>
2018-05-16 09:55:37 -0700

    about page beginnings

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import { Grid, Row, Col, FormControl, Button } from 'react-bootstrap'
+import Img from 'react-image'
 import CustomNavBar from './CustomNavBar'
 import Footer from './Footer'
 import AdminShortcut from './AdminShortcut'
-import { Grid, Row, Col, FormControl, Button } from 'react-bootstrap'
+import MichaelRoserImg from '../images/artist-portrait-michael-roser.JPG'
+import FredBriscoeImg from '../images/artist-portrait-fred-briscoe.JPG'
 
 class About extends Component {
 
@@ -31,6 +34,23 @@ class About extends Component {
             <h1>WHO IS {this.state.artistName.toUpperCase().replace('-', ' ')}?</h1>
           </div>
           <div className='about-content'>
+            <Col xs={12} md={6} className='about-image-col'>
+              <div className='about-image'>
+                <Img src={MichaelRoserImg} />
+              </div>
+            </Col>
+            <Col xs={12} md={6} className='about-desc'>
+              <p>Lorem ipsum dolor amet pug fam synth woke. Kale chips VHS brooklyn
+                man braid, before they sold out 90's woke tousled master cleanse
+                organic ugh health goth keytar pork belly marfa. Meditation hammock
+                pour-over, poke flexitarian roof party raclette live-edge deep v
+                bitters chartreuse intelligentsia. Fingerstache hammock paleo craft
+                beer taxidermy shaman keytar. Marfa actually wayfarers subway tile,
+                try-hard fam slow-carb bespoke humblebrag YOLO pabst. Literally
+                waistcoat hot chicken gentrify irony pour-over kitsch sartorial
+                vegan photo booth bicycle rights wayfarers pabst. Actually subway
+                tile tousled readymade locavore.</p>
+            </Col>
           </div>
         </Grid>
         <Footer />
